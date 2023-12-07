c10ad97ae186c8523142a4af6916880361613e64
arthur-carvalho-teixeira <acarvalho.objective@rededor.com.br>
2023-12-07 11:40:37 -0300

    Added logging

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main() {
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
   for (int j = 0; j < image_height; j++) {
+    std::clog << "\r Scanlines remaining: " << (image_height - j) << ' '
+              << std::flush;
     for (int i = 0; i < image_width; i++) {
       double r = double(i) / (image_width - 1);
       double g = double(j) / (image_height - 1);
@@ -19,4 +21,5 @@ int main() {
       std::cout << ir << ' ' << ig << ' ' << ib << '\n';
     }
   }
+  std::clog << "\nDone.         \n";
 }
